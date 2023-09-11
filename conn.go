e4985f9f67c966955e2df48e648d2abd893949f4
Ignacio Maldonado <ignacio.maldonado@usm.cl>
2023-09-11 19:16:37 -0300

    producer

---
+++ conn.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/streadway/amqp"
+)
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare("server_1", false, false, false, false, nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(q)
+
+	for {
+		err := ch.Publish("", "server_1", false, false,
+			amqp.Publishing{
+				Headers:     nil,
+				ContentType: "text/plain",
+				Body:        []byte("sent at" + time.Now().String()),
+			})
+
+		if err != nil {
+			break
+		}
+
+		time.Sleep(2 * time.Second)
+	}
+}
